b3a0201555519cd50c853bba9ed46d8c71d717b4
Vyas <sbvyas07@gmail.com>
2024-09-02 15:55:20 +0530

    basic routes implemented using separate controllers

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,23 +1,12 @@
 import express from 'express';
+import { login, logout,signup } from '../controllers/auth.controller.js';
 
 const router  = express.Router();
 
-router.get("/signup",(req,res)=>{
-    res.json({
-        data: "you hit signup endpoint",
-    });
-});
+router.get("/signup", signup );
 
-router.get("/login",(req,res)=>{
-    res.json({
-        data: "you hit login endpoint",
-    });
-});
+router.get("/login",login);
 
-router.get("/logout",(req,res)=>{
-    res.json({
-        data: "you hit logout endpoint",
-    });
-});
+router.get("/logout",logout);
 
 export default router;
