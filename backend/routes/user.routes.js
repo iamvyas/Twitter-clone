a7a758ea5f02b799ffa5c372615e98312cf2bfc4
Vyas <sbvyas07@gmail.com>
2024-11-04 16:49:58 +0530

    added follow unfollow functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 import User from "../models/user.model.js";
-import { getUserProfile } from "../controllers/user.controller.js";
+import { getUserProfile , followUnfollowUser} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username",protectRoute,getUserProfile);
 //router.get("/suggested",protectRoute,getUserProfile);
-//router.post("/follow",protectRoute,followUnfollowUser);
+router.post("/follow/:id",protectRoute,followUnfollowUser);
 //router.post("/update",protectRoute,updateUser);
 
 
