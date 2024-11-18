3bac9eec77807a0fee76170a1c6cd28f698e13ac
Vyas <sbvyas07@gmail.com>
2024-11-18 16:27:49 +0530

    update user and suggested user functionality added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Notification from "../models/notification.model.js";
+import bcrypt from "bcryptjs";
 
 export const getUserProfile = async (req,res) => {
     const { username } = req.params ;
@@ -63,4 +64,75 @@ export const followUnfollowUser =async (req,res) => {
         console.log("error in followUnfollowUser controller",error.message);
         return res.status(500).json({error: "internal server error"});
     }
+}
+
+export const getSuggestedUsers = async (req, res) => {
+	try {
+		const userId = req.user._id;
+
+		const usersFollowedByMe = await User.findById(userId).select("following");
+
+		const users = await User.aggregate([
+			{
+				$match: {
+					_id: { $ne: userId },
+				},
+			},
+			{ $sample: { size: 10 } },
+		]);
+
+		// 1,2,3,4,5,6,
+		const filteredUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id));
+		const suggestedUsers = filteredUsers.slice(0, 4);
+
+		suggestedUsers.forEach((user) => (user.password = null));
+
+		res.status(200).json(suggestedUsers);
+	} catch (error) {
+		console.log("Error in getSuggestedUsers: ", error.message);
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const updateUser = async (req, res) => {
+	const { fullName, email, username, currentPassword, newPassword, bio, link } = req.body;
+	let { profileImg, coverImg } = req.body;
+
+	const userId = req.user._id;
+
+	try {
+		let user = await User.findById(userId);
+		if (!user) return res.status(404).json({ message: "User not found" });
+
+		if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+			return res.status(400).json({ error: "Please provide both current password and new password" });
+		}
+
+		if (currentPassword && newPassword) {
+			const isMatch = await bcrypt.compare(currentPassword, user.password);
+			if (!isMatch) return res.status(400).json({ error: "Current password is incorrect" });
+			if (newPassword.length < 6) {
+				return res.status(400).json({ error: "Password must be at least 6 characters long" });
+			}
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(newPassword, salt);
+		}
+
+		user.fullName = fullName || user.fullName;
+		user.email = email || user.email;
+		user.username = username || user.username;
+		user.bio = bio || user.bio;
+		user.link = link || user.link;
+
+		user = await user.save();
+
+		// password should be null in response
+		user.password = null;
+
+		return res.status(200).json(user);
+	} catch (error) {
+		console.log("Error in updateUser: ", error.message);
+		res.status(500).json({ error: error.message });
+	}
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 import User from "../models/user.model.js";
-import { getUserProfile , followUnfollowUser} from "../controllers/user.controller.js";
+import { getUserProfile , followUnfollowUser ,getSuggestedUsers , updateUser  } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username",protectRoute,getUserProfile);
-//router.get("/suggested",protectRoute,getUserProfile);
+router.get("/suggested",protectRoute,getSuggestedUsers);
 router.post("/follow/:id",protectRoute,followUnfollowUser);
-//router.post("/update",protectRoute,updateUser);
+router.post("/update",protectRoute,updateUser);
 
 
 export default router;
